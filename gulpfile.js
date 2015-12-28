060b0970bcce8e5317194831a05f21df3332fdb4
Angel Valero Archiles <resfriaros@gmail.com>
2015-12-29 00:11:25 +0100

    Config Gulp with plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,112 @@
 'use strict'
 
 var gulp = require('gulp'),
+    path = {},
+    connect = require('gulp-connect'),
     jade = require('gulp-jade'),
     sass = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    minifycss = require('gulp-minify-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+
+    jadeFiles = [
+      './src/jade/layout.jade',
+      './src/jade/index.jade',
+    ],
+    cssFiles = [
+      './src/sass/reset.css',
+      './src/sass/main.scss',
+    ],
+//    jsFiles = [
+//
+//    ],
+
+    minHTML = './dist/index.html',
+    minCSS = './dist/css/main.min.css',
+    minJS = './dist/js/script.min.js',
+
+//    path.watch = {
+//      HTLM = 'dist/*.html',
+//      CSS = 'dist/css/*.css',
+//    JS = 'dist/js/*.js',
+//    };
+
+    pathDist = './',
+
     locals = {
               title : 'Venditus',
               link : 'http://venditus.se/',
               description : 'description of 156 characters maximum'
     }
 
-
-gulp.task('jade', () => {
-    return gulp.src('./*.jade')
-    .pipe(jade({
-        pretty: true,
-        locals: locals
-    }))
-    .pipe(gulp.dest('./'))
+// Jade To Html
+gulp.task('html', () => {
+  return gulp
+            .src( jadeFiles )
+            .pipe(jade({
+                        pretty: true,
+                        locals: locals
+            }))
+            .pipe( concat( minHTML ) )
+            .pipe(gulp.dest( pathDist ))
 })
 
 
-gulp.task('sass', () => {
-  return gulp.src('./src/sass/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/css'));
+// SASS To CSS
+gulp.task('css', () => {
+  gulp
+      .src( cssFiles )
+      .pipe( sass() )                       // Sass to CSS
+      .pipe( concat( minCSS ) )             // Concat
+      .pipe( autoprefixer ({                // Autoprefixer
+        browsers: ['last 2 version'],
+        cascade: false
+      }))
+//      .pipe ( minifycss() )                 // Minify-css
+      .pipe( gulp.dest( pathDist ) )
+})
+
+// Autoprefixer
+gulp.task('auto', () => {
+  return gulp
+            .src( minCSS )
+            .pipe( autoprefixer({
+              browsers: ['last 2 version'],
+              cascade: false
+            }))
+            .pipe( gulp.dest( './dist/css/' ) )
+})
+
+// Server
+gulp.task('server', () => {
+    connect.server({
+      root : './dist',
+      port : 3000,
+      livereload : true
+    })
+})
+
+// Reload
+gulp.task('reload', () => {
+    gulp
+        .src(['./dist/**/*.*'])
+        .pipe( connect.reload() )
+})
+
+// Watch
+gulp.task('watch', () => {
+  gulp
+      .watch(['./dist/**/*.*'], ['reload'])
 });
 
-gulp.task('watch', function () {
-  gulp.watch("./src/sass/*.scss"), ["sass"];
+// Watch in CSS and HTML
+gulp.task('html5', () => {
+  gulp
+      .watch(['./src/**/*.*'], ['css', 'html'])
 });
 
-gulp.task('default', ['sass', 'jade', 'watch']);
+// Start
+gulp.task('default', ['server', 'watch', 'html5'])
+
+// Default
+gulp.task('min', ['css', 'html']);
